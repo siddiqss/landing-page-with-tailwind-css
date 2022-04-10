3a02ce0d7ca41885bbd330d1b324f675068b6028
Talha <tstalhasiddique@gmail.com>
2022-04-10 16:13:00 +0500

    Initial Commit!

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        black: "#010101",
+        white: "#ffffff",
+        brightGray: "#393b54",
+        towerGray: "#a3bdbe",
+        cornflowerBlue: "#635ef6"
+      }
+    },
+  },
+  plugins: [],
+}
